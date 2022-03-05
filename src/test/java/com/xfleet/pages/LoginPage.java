66b072a879b623f47459cb7194cade4594de9206
haydarcalis <haydarcalis@gmail.com>
2022-03-05 19:49:57 +0300

    1-some locators and a method are to Gridpage 2-a method is added to LoginPage 3-ManageFiltersFeature added 4-ManageFilterStepDefs added

--- src/test/java/com/xfleet/pages/LoginPage.java
+++ src/test/java/com/xfleet/pages/LoginPage.java
@@ -82,5 +82,24 @@ public class LoginPage {
                 throw new RuntimeException("Unknown user type!");
         }
     }
+    public void loggedAs (String userType){
+        Driver.get().get(ConfigurationReader.get("url"));
+
+        String username ="";
+        String password ="";
+
+        if(userType.equals("driver")){
+            username = ConfigurationReader.get("driver_username");
+            password = ConfigurationReader.get("driver_password");
+        }else if(userType.equals("sales manager")){
+            username = ConfigurationReader.get("salesmanager_username");
+            password = ConfigurationReader.get("salesmanager_password");
+        }else if(userType.equals("store manager")){
+            username = ConfigurationReader.get("storemanager_username");
+            password = ConfigurationReader.get("storemanager_password");
+        }
+
+        new LoginPage().login(username,password);
+    }
 
 }
