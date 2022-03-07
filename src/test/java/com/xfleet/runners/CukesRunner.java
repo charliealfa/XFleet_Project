0a231085700aca966d53dc4e637ded6529ce4108
Dohman the B <dohmanburak@gmail.com>
2022-03-07 13:14:29 +0300

    creating FilterLastOdometer step definition class

--- src/test/java/com/xfleet/runners/CukesRunner.java
+++ src/test/java/com/xfleet/runners/CukesRunner.java
@@ -11,8 +11,8 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/xfleet/step_definitions",
-        dryRun = false,
-        tags = "@new"
+        dryRun = true,
+        tags = "@FLTAPS-1189"
 
 )
 public class CukesRunner {
